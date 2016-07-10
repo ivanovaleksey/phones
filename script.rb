2de2773e17d1d3678640df62bef771ce0e34dec1
Aleksey Ivanov <ialexxei@gmail.com>
2016-07-10 13:33:01 +0300

    complete script

--- script.rb
+++ script.rb
@@ -1,41 +1,24 @@
 #!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
 
 require 'mrdialog'
 require 'batch_factory'
 require 'axlsx'
 
 class Worker
-  MIN_DURATION = 10
+  attr_reader :viewer
 
-  def initialize(filename)
-    @filename = filename
-  end
+  class Parser
+    attr_reader :rows, :filename
+    MIN_DURATION = 10
 
-  def call
-    fetch_rows
-    p call_stats[110]
-  end
-
-  private
-
-  def fetch_rows
-    @rows ||= BatchFactory.from_file(@filename, keys: keys).rows.drop(1).select do |row|
-      row[:duration] > MIN_DURATION
-    end.map do |row|
-      row.each_with_object({}) do |(k, v), hash|
-        value = v.is_a?(Float) ? v.to_i : v
-        hash[k.to_sym] = value
-      end
+    def initialize(filename)
+      @filename = filename
     end
-  end
 
-  def calls_by_number
-    @calls_by_number ||= @rows.group_by { |row| row[:number] }
-  end
-
-  def call_stats
-    @call_stats ||= begin
-      stats = calls_by_number.each_with_object({}) do |(number, calls), hash|
+    def call_stats(type)
+      method_name = "#{type}_calls_by_number".to_sym
+      stats = send(method_name).each_with_object({}) do |(number, calls), hash|
         sorted_calls = calls.sort_by { |call| call[:datetime] }
         hash[number] = {
           main: {
@@ -47,18 +30,18 @@ class Worker
       end
 
       special_numbers.each do |number|
-        before_17 = calls_by_number[number].select { |call| call[:datetime].hour < 17 }.sort_by { |call| call[:datetime] }
-        after_17  = calls_by_number[number].select { |call| call[:datetime].hour >= 17 }.sort_by { |call| call[:datetime] }
+        before_17 = send(method_name)[number].select { |call| call[:datetime].hour < 17 }.sort_by { |call| call[:datetime] }
+        after_17  = send(method_name)[number].select { |call| call[:datetime].hour >= 17 }.sort_by { |call| call[:datetime] }
 
         stats[number] = {
           before_17: {
-            first: before_17.first[:datetime],
-            last:  before_17.last[:datetime],
+            first: before_17.first ? before_17.first[:datetime] : nil,
+            last:  before_17.last ? before_17.last[:datetime] : nil,
             count: before_17.count
           },
           after_17: {
-            first: after_17.first[:datetime],
-            last:  after_17.last[:datetime],
+            first: after_17.first ? after_17.first[:datetime] : nil,
+            last:  after_17.last ? after_17.last[:datetime] : nil,
             count: after_17.count
           }
         }
@@ -66,16 +49,156 @@ class Worker
 
       stats
     end
+
+    private
+
+    def rows
+      @rows ||= BatchFactory.from_file(@filename, keys: keys).rows.drop(1).select do |row|
+        row[:duration] > MIN_DURATION
+      end.map do |row|
+        row.each_with_object({}) do |(k, v), hash|
+          value = v.is_a?(Float) ? v.to_i : v
+          hash[k.to_sym] = value
+        end
+      end
+    end
+
+    def incoming_calls
+      @incoming_calls ||= rows.select { |call| inner_number?(call[:number]) || call[:number] == 'incoming' }
+    end
+
+    def incoming_calls_by_number
+      @incoming_calls_by_number ||= incoming_calls.group_by { |call| call[:number] }
+    end
+
+    def outgoing_calls
+      @outgoing_calls ||= rows.select { |call| inner_number? call[:sip] }
+    end
+
+    def outgoing_calls_by_number
+      @outgoing_calls_by_number ||= outgoing_calls.group_by { |call| call[:sip] }
+    end
+
+    def keys
+      [:id, :sip, :datetime, :clid, :number, :state, :duration]
+    end
+
+    def special_numbers
+      [110, 111]
+    end
+
+    def inner_number?(number)
+      (100...400).include? number
+    end
   end
 
-  def keys
-    [:id, :sip, :datetime, :clid, :number, :state, :duration]
+  class Viewer
+    attr_reader :parser, :styles
+
+    def initialize(parser)
+      @parser = parser
+    end
+
+    def call
+      Axlsx::Package.new do |p|
+        workbook = p.workbook
+        add_sheet workbook, { name: 'Incoming', type: :incoming }
+        add_sheet workbook, { name: 'Outgoing', type: :outgoing }
+        p.serialize 'results.xlsx'
+      end
+    end
+
+    private
+
+    def define_styles(sheet)
+      @styles ||= begin
+        centered = sheet.styles.add_style alignment: { horizontal: :center }
+        header   = sheet.styles.add_style alignment: { horizontal: :center }, bg_color: 'FFE699', border: { style: :thin, color: '000000', edges: [:top, :bottom, :left, :right] }
+        number   = sheet.styles.add_style bg_color: 'DDEBF7', border: { style: :thin, color: '000000', edges: [:top, :bottom, :left, :right] }
+        details  = sheet.styles.add_style bg_color: 'BDD7EE', border: { style: :thin, color: '000000', edges: [:top, :bottom, :left, :right] }
+
+        { centered: centered, header: header, phone_number: number, details: details }
+      end
+    end
+
+    def add_headers(sheet)
+      sheet.merge_cells 'C1:E1'
+      sheet.add_row [nil, nil, date], style: [nil, nil, styles[:header]]
+      # sheet.add_row [nil, nil, 'Week day'], style: styles[:header]
+      sheet.add_row [nil, nil, 'Кол-во', 'Первый зв.', 'Последний зв.'], widths: [:auto, :auto, 20, 20, 20], style: [nil, nil, styles[:header], styles[:header], styles[:header]]
+
+      # sheet.merge_cells 'C1:E1'
+      # sheet.merge_cells 'C2:E2'
+    end
+
+    def add_sheet(book, params)
+      book.add_worksheet(name: params[:name]) do |sheet|
+        define_styles sheet
+        add_headers sheet
+
+        Hash[parser.call_stats(params[:type]).sort_by { |k, _| k.to_i }].each do |number, stats|
+          if stats.has_key? :main
+            sheet.add_row [
+              number,
+              nil,
+              stats[:main][:count],
+              stats[:main][:first]&.strftime('%H:%M:%S'),
+              stats[:main][:last]&.strftime('%H:%M:%S'),
+            ], style: [styles[:phone_number], styles[:details], styles[:centered], styles[:centered], styles[:centered]]
+          else
+            sheet.add_row [number, nil], style: [styles[:phone_number], styles[:details], nil, nil, nil]
+            sheet.add_row [
+              nil,
+              'До 17:00',
+              stats[:before_17][:count],
+              stats[:before_17][:first]&.strftime('%H:%M:%S'),
+              stats[:before_17][:last]&.strftime('%H:%M:%S'),
+            ], style: [styles[:phone_number], styles[:details], styles[:centered], styles[:centered], styles[:centered]]
+            sheet.add_row [
+              nil,
+              'После 17:00',
+              stats[:after_17][:count],
+              stats[:after_17][:first]&.strftime('%H:%M:%S'),
+              stats[:after_17][:last]&.strftime('%H:%M:%S'),
+            ], style: [styles[:phone_number], styles[:details], styles[:centered], styles[:centered], styles[:centered]]
+          end
+        end
+
+        total = parser.call_stats(params[:type]).inject(0) do |count, (number, stats)|
+          if stats.has_key? :main
+            count + stats[:main][:count]
+          else
+            count + stats[:before_17][:count] + stats[:after_17][:count]
+          end
+        end
+        sheet.add_row [
+          nil,
+          'Сумма',
+          total
+        ], style: [styles[:phone_number], styles[:details], styles[:centered]]
+      end
+    end
+
+    def sort
+      -> (a, b) do
+        return -1 if a.is_a? String
+        a <=> b
+      end
+    end
+
+    def date
+      File.basename(parser.filename).split('.').first(3).join '/'
+    end
   end
 
-  def special_numbers
-    [110, 111]
+  def initialize(filename)
+    @parser = Parser.new(filename)
+    @viewer = Viewer.new(@parser)
   end
 
+  def call
+    viewer.call
+  end
 end
 
 class Dialog
